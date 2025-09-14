824d350249b4953c84dba3d9ff8df25bd3852665
AllanKeven <alencarmesquitaallankeven@gmail.com>
2025-09-14 11:12:07 -0300

    ajustando erros

--- routes/prod.routes.js
+++ routes/prod.routes.js
@@ -4,8 +4,9 @@ const {productsController} = require('../controller/productsController');
 const prodRoutes =  express.Router();
 
 
-prodRoutes.get('/', new productsController().getProd)/
+prodRoutes.get('/', new productsController().getProd);
 prodRoutes.post('/', new productsController().postProd);
-
+prodRoutes.put('/:id', new productsController().updateProd);
+prodRoutes.delete('/:id', new productsController().deleteProd);
 
 module.exports = prodRoutes;
