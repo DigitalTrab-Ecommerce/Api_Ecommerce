5277a32d3d013b25ad5bbad012c1d30bacb4194e
GomesNetoDEV <gomes.idb@gmail.com>
2025-09-18 18:01:27 -0300

    feat(index.js): adicionar ferramenta de obsevabilidade Sentry

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
 
 const express = require('express');
 const cors = require('cors')
+const Sentry = require("@sentry/node");// REQUIRE DO SENTRY
+require("dotenv").config(); // CHAVE DO SENTRY NO .ENV
 const port = 3001;
-const routes = require('./routes/index.routes')
-
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+// INICIANDO O SENTRY SEMPRE ANTES DAS ROTAS
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+});
+app.use(Sentry.Handlers.requestHandler());
+
+const routes = require('./routes/index.routes')
 app.use('/api', routes);
 
+// Rota de teste do Sentry
+const router = require("express").Router();
+router.get("/debug-sentry", (req, res) => {
+  throw new Error("Teste de erro no Sentry!");
+});
+app.use("/test", router);
+
+app.use(Sentry.Handlers.errorHandler());
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ error: "Ocorreu um erro inesperado!" });
+});
 
 
+const PORT = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Servidor rodando na porta: ${port}`)
 })
